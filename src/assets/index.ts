c1a718ffb8b85ec4a981a7712cad31b42ec85683
SivaPrakasam7 <prakasams22@gmail.com>
2022-02-13 20:52:26 +0530

    unused import removed

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,7 @@
 import CorrectedBG from "src/assets/corrected_bg.jpg";
 import NormalBg from "src/assets/normal_bg.jpg";
 import Bg from "src/assets/bg.jpg";
-import Me from "src/assets/me.jpg";
 import OldMe from "src/assets/old_me.jpg";
 import Resume from "src/assets/Siva Prakasam Ganesan.pdf";
 
-export { CorrectedBG, NormalBg, Bg, Me, OldMe, Resume };
+export { CorrectedBG, NormalBg, Bg, OldMe, Resume };
